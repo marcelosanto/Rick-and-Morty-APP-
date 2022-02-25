715d34fcad6efa3ffa774616d3331d9b9eabefaa
Marcelo Santos <rocha.mer21@gmail.com>
2022-02-25 14:23:50 -0300

    add MainFragment, add utils com funcionalidade de navegacao entre fragments e add view biding

---
+++ app/src/main/java/xyz/spacermarcelo/rickandmorty/MainFragment.kt
@@ -0,0 +1,19 @@
+package xyz.spacermarcelo.rickandmorty
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import xyz.spacermarcelo.rickandmorty.databinding.FragmentMainBinding
+
+
+class MainFragment : androidx.fragment.app.Fragment(R.layout.fragment_main) {
+
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMainBinding.bind(view)
+    }
+}
---
+++ app/src/main/java/xyz/spacermarcelo/rickandmorty/utils/FrangmentExt.kt
@@ -0,0 +1,12 @@
+package xyz.spacermarcelo.rickandmorty.utils
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.IdRes
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+
+fun Fragment.navTo(@IdRes dest: Int) = findNavController().navigate(dest)
+fun Fragment.navTo(@IdRes dest: Int, args: Bundle) = findNavController().navigate(dest, args)
+fun Fragment.toast(msg: String) = Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
