e159bab33c68e8a61ad1bda0b90cc0a6ee7fca80
Marcelo Santos <rocha.mer21@gmail.com>
2022-03-02 22:08:06 -0300

    criado o detail fragrment e add navegacao e a tela

--- app/src/main/java/xyz/spacermarcelo/rickandmorty/MainFragment.kt
+++ app/src/main/java/xyz/spacermarcelo/rickandmorty/MainFragment.kt
@@ -1,11 +1,9 @@
 package xyz.spacermarcelo.rickandmorty
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import xyz.spacermarcelo.rickandmorty.databinding.FragmentMainBinding
+import xyz.spacermarcelo.rickandmorty.utils.navTo
 
 
 class MainFragment : androidx.fragment.app.Fragment(R.layout.fragment_main) {
@@ -15,5 +13,9 @@ class MainFragment : androidx.fragment.app.Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMainBinding.bind(view)
+
+        binding.btnFilter.setOnClickListener {
+            navTo(R.id.filterFragment)
+        }
     }
 }
